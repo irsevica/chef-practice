e95e146670bdf3547863b150834a31b22996a7de
AnitaI <anita.irsevic@gmail.com>
2018-12-01 11:07:49 +0000

    add nginx setup code

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,11 @@
-#
-# Cookbook:: bcf_nginx
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
+package "nginx"
+
+service "nginx" do
+  action [:enable, :start]
+end
+
+file "/usr/share/nginx/html/index.html" do
+  action :create
+  content "<h1>Hello World</h1>"
+  not_if { ::File.exists?("/usr/share/nginx/html/index.html") }
+end
